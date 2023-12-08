321ec9d76bebe2e9905cbcec558b370d8ce2dadc
Nils Baxheinrich <nils@nbax.de>
2023-12-08 14:08:53 +0100

    Test der Mul

--- Hadoop/hadoopjoin/src/main/java/hadoopjoin/src/main/java/de/nbax/hadoopjoin/JoinDriver.java
+++ Hadoop/hadoopjoin/src/main/java/hadoopjoin/src/main/java/de/nbax/hadoopjoin/JoinDriver.java
@@ -1,28 +1,76 @@
 package hadoopjoin.src.main.java.de.nbax.hadoopjoin;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JoinDriver {
+    public static class ProductMapper
+            extends Mapper<Object, Text, Text, Text> {
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String record = value.toString();
+            String[] parts = record.split("\\|");
+            if (parts.length > 4) {
+                context.write(new Text(parts[2]), new Text("PROD|" + parts[1] + '|' +
+                        parts[5]));
+            }
+        }
+    }
+
+    public static class SupplierMapper
+            extends Mapper<Object, Text, Text, Text> {
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+            String record = value.toString();
+            String[] parts = record.split("\\|");
+            context.write(new Text(parts[0]), new Text("SUPP|" + parts[1] + '|' + parts[5] +
+                    '|' + parts[8]));
+        }
+    }
+
+    public static class ReduceJoinReducer
+            extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            ArrayList<String> prod = new ArrayList<String>();
+            ArrayList<String> supp = new ArrayList<String>();
+            for (Text t : values) {
+                String parts[] = t.toString().split("\\|");
+                if (parts[0].equals("PROD")) {
+                    prod.add(parts[1] + '|' + parts[2]);
+                } else if (parts[0].equals("SUPP")) {
+                    supp.add(parts[1] + '|' + parts[2] + '|' + parts[3]);
+                }
+            }
+            for (String p : prod) {
+                for (String s : supp) {
+                    context.write(key, new Text(p + "|" + s));
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "reduce-side join");
         job.setJarByClass(JoinDriver.class);
-        job.setReducerClass(JoinReducer.class);
+        job.setReducerClass(ReduceJoinReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-
-        // Input paths for tableA.csv and tableB.csv
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, JoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, JoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class,
+                ProductMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class,
+                SupplierMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-
 }
